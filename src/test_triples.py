432bc0d1bcc8955a7a525f8cae9536fbf2395078
jiqirui <1085436942@qq.com>
2023-10-01 17:20:18 +0800

    a

--- src/test_triples.py
+++ src/test_triples.py
@@ -18,7 +18,9 @@ parser = argparse.ArgumentParser(description='JOIE Testing: Type Linking')
 parser.add_argument('--modelname', type=str, help='model category', default='./dbpedia')
 parser.add_argument('--model', type=str, help='model name including data and model',
                     default="transe_CMP-double_dim1_300_dim2_100_a1_2.5_a2_1.0_m1_0.5_fold_3")
-parser.add_argument('--testfile', type=str, help='test data', default="../data/dbpedia/db_InsType_test.txt")
+parser.add_argument('--task', type=str, help="tasks", choices=["triple-completion", "entity-typing"],
+                    default="triple-completion")
+parser.add_argument('--testfile', type=str, help='test data', default="../data/dbpedia/db_onto_small_test.txt")
 parser.add_argument('--method', type=str, help='embedding method used')
 parser.add_argument('--resultfolder', type=str, help='result output folder', default="dbpedia")
 parser.add_argument('--graph', type=str, help='test which graph (ins/onto)', default="onto")
@@ -47,8 +49,10 @@ max_check = 100000
 
 tester = Tester()
 tester.build(save_path=model_file, data_save_path=data_file, graph=args.graph, method=args.method, bridge=args.bridge)
-# tester.load_test_link(test_data, max_num=max_check, splitter='\t', line_end='\n')
-tester.load_cross_link(test_data, max_num=max_check, splitter='\t', line_end='\n')
+if args.task == "triple-completion":
+    tester.load_test_link(test_data, max_num=max_check, splitter='\t', line_end='\n')
+else:
+    tester.load_cross_link(test_data, max_num=max_check, splitter='\t', line_end='\n')
 
 def triple_completion(tester):
     ranks = []
@@ -164,104 +168,7 @@ def entity_typing(tester):
     return mrr, hit_1, hit_3
 
 
-
-
-# triple_completion(tester)
-entity_typing(tester)
-
-
-# index = 0  # index
-# rst_predict = []  # scores for each case
-# rank_record = []
-# prop_record = []
-#
-# t0 = time.time()
-#
-#
-# def triple_completion(tester):
-#     ranks = []
-#     hit_1 = 0
-#     hit_10 = 0
-#
-#     test_triples = tester.test_triples
-#
-#     for triple in test_triples:
-#         h, r, t = triple
-#         dist = tester.dist_source(h, r, t, source=args.method)
-#         ent_num = tester.joie.num_entsA if tester.graph_id == 1 else tester.joie.num_entsB
-#         all_ent = np.arange(ent_num)
-#         dist_list = tester.dist_source(h, r, all_ent)
-#         tail_rank = np.sum(dist_list <= dist)
-#         ranks.append(tail_rank)
-#
-#         if tail_rank <= 1:
-#             hit_1 += 1
-#         if tail_rank <= 10:
-#             hit_10 += 1
-#
-#     mrr = np.mean(1/np.array(ranks))
-#     hit_1 = hit_1 / len(test_triples)
-#     hit_10 = hit_10 / len(test_triples)
-#
-#     return mrr, hit_1, hit_10
-#
-#
-# def test(tester, index, rst_predict, rank_record, prop_record):
-#     while index < len(tester.test_triples):
-#         idx = index
-#         index += 1
-#         if idx > 0 and idx % 10 == 0:
-#             print("Tested %d in %d seconds." % (idx + 1, time.time() - t0))
-#             try:
-#                 print(np.mean(rst_predict, axis=0))
-#             except:
-#                 pass
-#         e1, r, e2 = tester.test_triples[idx]
-#
-#         # triple completion
-#         # vec = tester.ent_index2vec(e1, tester.graph_id)
-#         # vec_pool = tester.vec_e[tester.graph_id]
-#
-#         vec = tester.projection(e1, 1, bridge=args.bridge)
-#         vec_pool = tester.projection_pool(tester.vec_e[2])
-#
-#         rst = tester.kNN(vec, vec_pool, topK)
-#
-#         this_hit = []
-#         hit = 0.
-#         strl = tester.ent_index2str(rst[0][0], tester.graph_id)
-#         strr = tester.ent_index2str(e2, tester.graph_id)
-#         this_index = 0
-#         this_rank = None
-#         for pr in rst:
-#             this_index += 1
-#             if (hit < 1. and (pr[0] == e2 or pr[0] in tester.test_hr_map[e1][r])) or (
-#                     hit < 1. and tester.ent_index2str(pr[0], tester.graph_id) == strr):
-#                 hit = 1.
-#                 this_rank = this_index
-#             this_hit.append(hit)
-#         hit_first = 0
-#         if rst[0][0] == e2 or rst[0][0] in tester.test_hr_map[e1][r] or strl == strr:
-#             hit_first = 1
-#         if this_rank is None:
-#             this_rank = tester.rank_index_from(vec, vec_pool, e2)
-#         if this_rank > max_check:
-#             continue
-#         rst_predict.append(np.array(this_hit))
-#         rank_record.append(1.0 / (1.0 * this_rank))
-#         prop_record.append((hit_first, rst[0][1], strl, strr))
-#
-#
-# test(tester, index, rst_predict, rank_record, prop_record)
-# hits = np.mean(rst_predict, axis=0)
-# mean_rank = np.mean(rank_record)
-#
-# print(hits)
-# print(mean_rank)
-#
-# # print out result file
-# fp = open(result_file, 'w')
-# fp.write("Hits@" + str(topK) + '\n')
-# print(hits)
-# fp.write(' '.join([str(x) for x in hits]) + '\n')
-# fp.close()
+if args.task == "triple-completion":
+    triple_completion(tester)
+elif args.task == "entity-typing":
+    entity_typing(tester)
